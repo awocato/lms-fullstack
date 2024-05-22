f0cf523d33af905904e7ccf5425b75ee9bd7373c
awocato <minikhanov.talgat@gmail.com>
2024-05-22 22:46:28 +0500

    Update course-progress-button.tsx
    type fix

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -76,7 +76,7 @@ export const CourseProgressButton = ({
             onClick={onClick}
             disabled={isLoading}
             type="button"
-            variant={isCompleted ? "outline" : "success"}
+            variant={isCompleted ? "outline" : "secondary"}
             className="w-full md:w-auto"
         >
             {isCompleted ? "Not completed" : "Mark as complete"}
